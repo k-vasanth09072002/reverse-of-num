6cc6ee7e0b475e539ae75c8bc80bc370ef3a0216
k-vasanth09072002 <vasanthvasath74@gmail.com>
2022-12-22 16:55:05 +0530

    reverse-number-order-vaice

---
+++ main.js
@@ -0,0 +1,21 @@
+
+  let a=parseInt(prompt("enter number"));
+   var b=0;
+   var c=0;
+   
+
+//    while(a>0){
+//         b=a%10;
+//         a=Math.floor(a/10);
+
+//     c++
+//     console.log(b);
+//    }
+   
+
+for(i=0;a!=0;i++){
+     b=a%10;
+     a=Math.floor(a/10);
+     console.log(b);
+}
+    
